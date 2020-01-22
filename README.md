8fa5ebf7efc5e73657783367d4b9c121324adcbd
k/bit <biters.k@gmail.com>
2020-01-23 01:40:19 +0200

    Add code
    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # unity-360-video-player
 Simple unity c# script to play 360 video in unity
+
+# UNITY 360 VIDEO PLAYER
+
+#### Simple script to play 360 video in unity.
+Put the script on main camera and change the parameters.
+
+- ###### use original video resolution 
+- ###### change video resolution
+- ###### loop video
+- ###### mute sound
---
+++ VideoPlayer360.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+// k/bit
+
+public class VideoPlayer360 : MonoBehaviour {
+
+    RenderTexture rendererTexture;
+    Material skyboxMaterial;
+    public VideoClip videoClip;
+    public bool useVideoResolution = true;
+    public int rendererTextureWidth = 256;
+    public int rendererTextureHeight = 256;
+    public bool videoLoop = true;
+    public bool muteSound = false;
+
+    void Start () {
+
+        // create texture renderrer
+        if (useVideoResolution) {
+            rendererTexture = new RenderTexture ((int) videoClip.width, (int) videoClip.height, 16, RenderTextureFormat.ARGB32);
+        } else {
+            rendererTexture = new RenderTexture (rendererTextureWidth, rendererTextureHeight, 16, RenderTextureFormat.ARGB32);
+        }
+        rendererTexture.Create ();
+
+        // create skybox/panoram material  and set it in render settings
+        skyboxMaterial = new Material (Shader.Find ("Skybox/Panoramic"));
+        skyboxMaterial.mainTexture = rendererTexture;
+        RenderSettings.skybox = skyboxMaterial;
+
+        // create video player
+        AudioSource audioSource = GetComponent<AudioSource> ();
+        VideoPlayer videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer> ();
+        videoPlayer.playOnAwake = true;
+        videoPlayer.isLooping = videoLoop;
+        videoPlayer.clip = videoClip;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+        videoPlayer.targetTexture = rendererTexture;
+        if (muteSound) {
+            videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.None;
+        } else {
+            videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.Direct;
+        }
+    }
+}
